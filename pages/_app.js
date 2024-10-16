962aca8385601693053eb446be882be1e264c8cb
abilashv28 <abilashv281999@gmail.com>
2024-10-17 00:02:01 +0530

    logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from 'next/head'; // Import the Head component
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/student.png" type="image/png" sizes="32x32" /> {/* Link to your student.png */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
